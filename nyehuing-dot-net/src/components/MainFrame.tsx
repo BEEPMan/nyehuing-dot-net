26090dd6b3a8c872111b2ac7896a5fe664221f0b
eevee <jujh97@ajou.ac.kr>
2020-05-15 14:28:24 +0900

    create first main page

---
+++ nyehuing-dot-net/src/components/MainFrame.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './public/Style.css'
+
+function MainFrame() {
+  return (
+    <div>
+      <div className="MainSubTitle">
+        하찮았지만 귀찮았던것을 대신 해주는 사이트.
+      </div>
+      <div className="MainTitle">
+        녜힁 닷넷
+      </div>
+    </div>
+  )
+}
+
+export default MainFrame;
