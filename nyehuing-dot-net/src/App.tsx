259870e5b8d87c6b6850f6302bb45cd33fb092d6
정진원 <jinwon.jung@ngine.co.kr>
2020-05-15 18:37:24 +0900

    router 변경

--- nyehuing-dot-net/src/App.tsx
+++ nyehuing-dot-net/src/App.tsx
@@ -1,9 +1,22 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from 'react-router-dom';
 import MainFrame from './components/MainFrame';
 
 function App() {
   return (
-    <MainFrame></MainFrame>
+    <div>
+      <Router>
+        <Switch>
+          <Route path="/" exact component={MainFrame} />
+          <Redirect to="/" />
+        </Switch>
+      </Router>
+    </div>
   );
 }
 
--- nyehuing-dot-net/src/components/MainFrame.tsx
+++ nyehuing-dot-net/src/components/MainFrame.tsx
@@ -1,21 +1,43 @@
 import React from 'react';
+import {
+  Link,
+} from 'react-router-dom';
+import {
+  makeStyles,
+  Theme,
+  createStyles,
+} from '@material-ui/core/styles';
 import MainMenu from './MainMenu';
 import MainPage from './MainPage';
 import './public/Style.css';
 
-function MainFrame() {
+const useStyle = makeStyles((theme: Theme) => 
+  createStyles({
+    MainTitle: {
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      fontFamily: 'CookieRunBold',
+      color: '#fff',
+      fontSize: 45,
+    },
+  }),
+);
+
+const MainFrame: React.FC = () => {
+
+  const classes = useStyle();
+
   return (
     <div>
-      <a href="/">
-        <div className='MainTitle'>
-          <img src='./icon.png' id='icon'></img>
-          녜힁 닷넷
-        </div>
-      </a>
+      <Link to="/" className={classes.MainTitle}>
+        <img src='./icon.png' id='icon'/>
+        녜읭 닷넷
+      </Link>
       <MainMenu></MainMenu>
       <MainPage></MainPage>
     </div>
   )
-}
+};
 
 export default MainFrame;
