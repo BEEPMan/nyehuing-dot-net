04965e40df89b71d779cb94560639ff1775d47e2
beepman <dndbaktzhffk@gmail.com>
2020-05-24 14:29:12 +0900

    checking

--- nyehuing-dot-net/src/components/Body.tsx
+++ nyehuing-dot-net/src/components/Body.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
-import { Home, Nyehuing } from '../containers';
+import { Home, Nyehuing, BossCalculator } from '../containers';
 
 const Body: React.FC = () => {
   return (
     <div>
       <Switch>
         <Route path="/nyehuing" component={Nyehuing} />
+        <Route path="/calculator" component={BossCalculator} />
         <Route path="/" component={Home} />
       </Switch>
     </div>
--- nyehuing-dot-net/src/containers/BossCalculator.tsx
+++ nyehuing-dot-net/src/containers/BossCalculator.tsx
@@ -1,9 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
+
+const getBoss = async () => {
+  try {
+    return await axios.get('http://127.0.0.1:3010/calculator');
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+const countBreeds = async () => {
+  const boss = await getBoss();
+  if(boss?.data) {
+    console.log(boss);
+  }
+};
 
 const BossCalculator: React.FC = () => {
+  countBreeds();
   return (
     <div>
-
+      <h2>★보스 결정석 계산기★</h2>
     </div>
   );
 };
